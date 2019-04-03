9d64ea4833201a5624f2b715749325ec1a090394
Jake Shams <jshams62@gmail.com>
2019-04-03 13:18:31 -0700

    small optimizations and fixed an error

--- bases.py
+++ bases.py
@@ -1,6 +1,7 @@
 #!python
 
 import string
+from math import floor, log
 # Hint: Use these string constants to encode/decode hexadecimal digits and more
 # string.digits is '0123456789'
 # string.hexdigits is '0123456789abcdefABCDEF'
@@ -8,9 +9,10 @@ import string
 # string.ascii_uppercase is 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 # string.ascii_letters is ascii_lowercase + ascii_uppercase
 # string.printable is digits + ascii_letters + punctuation + whitespace
-string_of_digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+string_of_digits = string.digits + string.ascii_lowercase
 
-def decode(digits, base):
+
+def decode(digits: str, base: int) -> int:
     """Decode given digits in given base to number in base 10.
     digits: str -- string representation of number (in given base)
     base: int -- base of given number
@@ -18,29 +20,23 @@ def decode(digits, base):
     # Handle up to base 36 [0-9a-z]
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
 
-    digits = digits.upper()
+    digits = digits.lower()
 
-    
+    # quick optimization
     if base == 10:
-        return digits
+        return int(digits)
+
+    # this will increment through the loop
+    # i will increment for each exponent
     output = 0
-    i = 0
-    for digit in reversed(digits):
+    for i, digit in enumerate(reversed(digits)):
         output += string_of_digits.index(digit) * (base ** i)
-        i += 1
     # print(output)
     return output
 
-    
-    # TODO: Decode digits from binary (base 2)
-    # ...
-    # TODO: Decode digits from hexadecimal (base 16)
-    # ...
-    # TODO: Decode digits from any base (2 up to 36)
-    # ...
 
 
-def encode(number, base):
+def encode(number: int, base: int) -> str:
     """Encode given number in base 10 to digits in given base.
     number: int -- integer representation of number (in base 10)
     base: int -- base to convert to
@@ -49,38 +45,30 @@ def encode(number, base):
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
     # Handle unsigned numbers only for now
     assert number >= 0, 'number is negative: {}'.format(number)
-    # TODO: Encode number in binary (base 2)
-    # ...
-    # TODO: Encode number in hexadecimal (base 16)
-    # ...
-    # TODO: Encode number in any base (2 up to 36)
-    # ...
 
-    i = 0
-    while True:
-        if base ** i > number:
-            i -= 1
-            break
-        i += 1
-
+    # this finds the exponent to start counting down with
+    power = floor(log(number, base))
+    # output will be added to this string
     output = ''
+    # this loops through each exponent subtracting the value from the number and adding to the string
     while number != 0:
-        exponential_number = base ** i
+        exponential_number = base ** power
         new_digit_index = 0
-        while number >= exponential_number:
-            number -= exponential_number
-            new_digit_index += 1
+        # while number >= exponential_number:
+        #     number -= exponential_number
+        #     new_digit_index += 1
+        new_digit_index = floor(number / exponential_number)
+        number = number % exponential_number
         new_digit = string_of_digits[new_digit_index]
         output += new_digit
-        i -= 1
+        power -= 1
+        if number == 0:
+            for i in range(power + 1):
+                output += "0"
 
-    # print(output)
-    
+    print(output)
+    # output = "hello"
     return output
-
-
-
-
 
 def convert(digits, base1, base2):
     """Convert given digits in base1 to digits in base2.
@@ -100,7 +88,6 @@ def convert(digits, base1, base2):
     # TODO: Convert digits from base 10 to base 16 (and vice versa)
     # ...
     # TODO: Convert digits from any base to any base (2 up to 36)
-    # ...
 
 
 def main():
@@ -115,7 +102,7 @@ def main():
         result = convert(digits, base1, base2)
         print('{} in base {} is {} in base {}'.format(digits, base1, result, base2))
     elif len(args) == 2:
-        decode(args[0], int(args[1]))
+        encode(int(args[0]), int(args[1]))
     else:
         print('Usage: {} digits base1 base2'.format(sys.argv[0]))
         print('Converts digits from base1 to base2')
