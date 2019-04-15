93afbad524c4c6bad51d60553b646516bb5ffeba
Jake Shams <jshams62@gmail.com>
2019-04-14 19:19:18 -0700

    added deque class and implemented methods

--- queue.py
+++ queue.py
@@ -1,6 +1,7 @@
 #!python
 
 from linkedlist import LinkedList
+from doublylinkedlist import DoublyLinkedList
 
 
 # Implement LinkedQueue below, then change the assignment at the bottom
@@ -97,6 +98,76 @@ class ArrayQueue(object):
             raise ValueError('Queue is empty')
         self.size -= 1
         return self.list.pop(0)
+
+
+class Deque(object):
+    def __init__(self, iterable=None):
+        """Initialize this queue and enqueue the given items, if any."""
+        # Initialize a new linked list to store the items
+        self.list = DoublyLinkedList()
+        if iterable is not None:
+            for item in iterable:
+                self.enqueue(item)
+
+    def __repr__(self):
+        """Return a string representation of this queue."""
+        return 'Queue({} items, front={})'.format(self.length(), self.front())
+
+    def is_empty(self):
+        """Return True if this queue is empty, or False otherwise."""
+        return self.list.head is None
+
+    def length(self):
+        """Return the number of items in this queue."""
+        return self.list.size
+
+    def push_front(self, item):
+        """Insert the given item at the front of this queue.
+        Running time: O(1)"""
+        self.list.prepend(item)
+
+    def push_back(self, item):
+        """Insert the given item at the back of this queue.
+        Running time: O(1) – adding a new node with a linked list takes O(1) time"""
+        self.list.append(item)
+
+    def front(self):
+        """Return the item at the front of this queue without removing it,
+        or None if this queue is empty."""
+        if self.is_empty():
+            return None
+        return self.list.head.data
+    
+    def back(self):
+        """Return the item at the back of this queue without removing it,
+        or None if this queue is empty."""
+        if self.is_empty():
+            return None
+        return self.list.tail.data
+
+    def pop_front(self):
+        """Remove and return the item at the front of this queue,
+        or raise ValueError if this queue is empty.
+        Running time: O(1) – removing an item from the front of a linked list takes O(1)"""
+        if self.is_empty():
+            raise ValueError('Queue us empty')
+        data = self.list.head.data
+        self.list.head = self.list.head.next
+        self.list.size -= 1
+        return data
+
+    def pop_back(self):
+        """Remove and return the item at the back of this queue, 
+        or raise ValueError if this queue is empty.
+        Running time: O(1)"""
+        if self.is_empty():
+            raise ValueError('Queue is empty')
+        data = self.list.tail.data
+        self.list.tail = self.list.tail.prev
+        self.list.size -= 1
+        return data
+
+
 
 
 # Implement LinkedQueue and ArrayQueue above, then change the assignment below
