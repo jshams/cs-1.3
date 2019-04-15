f8cd078be3b9c8f66765eebf1576db8d5ea3ec28
Jake Shams <jshams62@gmail.com>
2019-04-14 17:47:32 -0700

    annoted time complexities

--- stack.py
+++ stack.py
@@ -20,26 +20,26 @@ class LinkedStack(object):
         return 'Stack({} items, top={})'.format(self.length(), self.peek())
 
     def is_empty(self):
-        """Return True if this stack is empty, or False otherwise."""
-        # TODO: Check if empty
+        """Return True if this stack is empty, or False otherwise.
+        Running time O(1)"""
         return self.list.head is None
 
 
     def length(self):
-        """Return the number of items in this stack."""
-        # TODO: Count number of items
+        """Return the number of items in this stack.
+        Running time: O(1)
+        """
         return self.list.size
 
     def push(self, item):
         """Insert the given item on the top of this stack.
-        Running time: O(1) – Why? [TODO]"""
-        # TODO: Push given item
+        Running time: O(1) – because were just creating a new node, no shifting necessary"""
         self.list.prepend(item)
 
     def peek(self):
         """Return the item on the top of this stack without removing it,
-        or None if this stack is empty."""
-        # TODO: Return top item, if any
+        or None if this stack is empty. 
+        Running time: O(1)"""
         if self.is_empty():
             return None
         return self.list.head.data
@@ -47,8 +47,7 @@ class LinkedStack(object):
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return top item, if any
+        Running time: O(1) because were just changing the value of the head. no shift necessary"""
         if self.is_empty():
             raise ValueError('Stack is empty')
         data = self.list.head.data
