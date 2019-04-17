72c01718cfefaada817612f4ad16407011674eb9
Jake Shams <jshams62@gmail.com>
2019-04-17 15:02:38 -0700

    tests are functional and passing

--- set_test.py
+++ set_test.py
@@ -5,47 +5,53 @@ class SetTest(unittest.TestCase):
 
     def test_init(self):
         test_set = Set(['A','B','C','D','E'])
-        assert = test_set.length() == 5
-        assert test_set.test_contains('A') == True
-        assert test_set.test_contains('C') == True
-        assert test_set.test_contains('E') == True
+        assert test_set.length() == 5
+        assert test_set.contains('A') == True
+        assert test_set.contains('C') == True
+        assert test_set.contains('E') == True
 
     def test_contains(self):
         test_set = Set(['A','B','C','D','E'])
-        assert = test_set.length() == 5
-        assert test_set.test_contains('A') == True
-        assert test_set.test_contains('B') == True
-        assert test_set.test_contains('C') == True
-        assert test_set.test_contains('D') == True
-        assert test_set.test_contains('E') == True
-        assert test_set.test_contains('F') == False
-        assert test_set.test_contains('Z') == False
+        assert test_set.length() == 5
+        assert test_set.contains('A') == True
+        assert test_set.contains('B') == True
+        assert test_set.contains('C') == True
+        assert test_set.contains('D') == True
+        assert test_set.contains('E') == True
+        assert test_set.contains('F') == False
+        assert test_set.contains('Z') == False
 
     def test_add(self):
         test_set = Set()
         assert test_set.length() == 0
         test_set.add('A')
-        assert test_set.test_contains('A') == True
+        assert test_set.contains('A') == True
         test_set.add('B')
-        assert test_set.test_contains('B') == True
+        assert test_set.contains('B') == True
         test_set.add('C')
-        assert test_set.test_contains('C') == True
+        assert test_set.contains('C') == True
     
     def test_remove(self):
-        test_set = Set(['A','B','C','D','E'])
-        assert test_set.length() == 5
+        test_set = Set(['A','B','C'])
+        assert test_set.length() == 3
         test_set.remove('A')
-        assert test_set.length() == 4
-        assert test_set.test_contains('A') == False 
-
+        assert test_set.length() == 2
+        assert test_set.contains('A') == False 
+        test_set.remove('B')
+        assert test_set.length() == 1
+        assert test_set.contains('B') == False
+        test_set.remove('C')
+        assert test_set.length() == 0
+        assert test_set.contains('C') == False
 
     def test_union(self):
-    
+        pass
     def test_intersection(self):
-
+        pass
     def test_difference(self):
-
+        pass
     def test_is_subset(self):
+        pass
 
     
 
