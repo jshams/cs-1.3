aebc582b2e418652a39c01b089a10989c82b5853
Jake Shams <jshams62@gmail.com>
2019-04-17 14:56:38 -0700

    added more tests

--- set_test.py
+++ set_test.py
@@ -4,12 +4,40 @@ import unittest
 class SetTest(unittest.TestCase):
 
     def test_init(self):
+        test_set = Set(['A','B','C','D','E'])
+        assert = test_set.length() == 5
+        assert test_set.test_contains('A') == True
+        assert test_set.test_contains('C') == True
+        assert test_set.test_contains('E') == True
 
     def test_contains(self):
+        test_set = Set(['A','B','C','D','E'])
+        assert = test_set.length() == 5
+        assert test_set.test_contains('A') == True
+        assert test_set.test_contains('B') == True
+        assert test_set.test_contains('C') == True
+        assert test_set.test_contains('D') == True
+        assert test_set.test_contains('E') == True
+        assert test_set.test_contains('F') == False
+        assert test_set.test_contains('Z') == False
 
     def test_add(self):
+        test_set = Set()
+        assert test_set.length() == 0
+        test_set.add('A')
+        assert test_set.test_contains('A') == True
+        test_set.add('B')
+        assert test_set.test_contains('B') == True
+        test_set.add('C')
+        assert test_set.test_contains('C') == True
     
     def test_remove(self):
+        test_set = Set(['A','B','C','D','E'])
+        assert test_set.length() == 5
+        test_set.remove('A')
+        assert test_set.length() == 4
+        assert test_set.test_contains('A') == False 
+
 
     def test_union(self):
     
