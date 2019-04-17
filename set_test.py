749f3d676aa4554d922e1d3f6939057cecc03d1a
Jake Shams <jshams62@gmail.com>
2019-04-17 13:46:57 -0700

    layed out the foundation

---
+++ set_test.py
@@ -0,0 +1,25 @@
+from set import Set
+import unittest
+
+class SetTest(unittest.TestCase):
+
+    def test_init(self):
+
+    def test_contains(self):
+
+    def test_add(self):
+    
+    def test_remove(self):
+
+    def test_union(self):
+    
+    def test_intersection(self):
+
+    def test_difference(self):
+
+    def test_is_subset(self):
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
