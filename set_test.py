846858c7afa8bd95a691a61eceb265a3f070e199
Jake Shams <jshams62@gmail.com>
2019-04-21 18:40:08 -0700

    added tests for intersection

--- set_test.py
+++ set_test.py
@@ -56,7 +56,13 @@ class SetTest(unittest.TestCase):
         self.assertCountEqual(union_items, ['A', 'B', 'C', 'D', 'E', 'F', 'G'])
 
     def test_intersection(self):
-        pass
+        test_set = Set(['A','B','C','D','E'])
+        other_set = Set(['C','D','E','F','G'])
+        intersection = test_set.intersection(other_set)
+        intersection_items = intersection.get_items()
+        print(intersection_items)
+        self.assertCountEqual(intersection_items, ['C', 'D', 'E'])
+
     def test_difference(self):
         pass
     def test_is_subset(self):
