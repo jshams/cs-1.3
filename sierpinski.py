0b428ac7cfae01943140472d36dd12d0bd526d3a
Jake Shams <jshams62@gmail.com>
2019-05-04 20:17:48 -0700

    added functions for blogpost

--- sierpinski.py
+++ sierpinski.py
@@ -2,14 +2,16 @@
 This program uses the turtle module. 
 The tutrtle module allows you to draw on a canvas
 turtle methods include:
-    fd() # will go forwards
-    bk() # will go backwards
-    lt(angle = 90) # turns left
-    rt(90) # turns right
-    pu() # pen up
-    pd() # pen down
-    setheading() # sets ts the direction (180 is left)
-    speed() # sets the speed, (1 - 10)
+	import turtle # imports turtle
+	pen = turtle.Turtle() #creates an instance of turtle
+    pen.fd() # will go forwards 
+    pen.bk() # will go backwards
+    pen.lt(angle) # turns left default 90
+    pen.rt(angle) # turns right default 90
+    pen.pu() # pen up
+    pen.pd() # pen down
+    pen.setheading(angle) # sets ts the direction (180 is left)
+    pen.speed(n) # sets the speed, (1 - 10)
 '''
 
 import turtle  
@@ -42,12 +44,64 @@ def sierpinski(n, width = 30):
         sierpinski(n - 1, width)
         pen.fd(width * 2 ** (n - 2))
 
-def eqilateral_triangle(l):
-    for i in range(3):
-        spen.rt(120)
-        spen.fd(l)
+def draw_triangle(length):
+    pen.setheading(180) # set the direction of the pen to left
+    for i in range(3): # draw 3 sides
+        pen.rt(120) # rotate the pen 120 degrees clockwise
+        pen.fd(length) # draw side
+	# pen will end up facing left (180)
 
+def sierpinski_order_two(length):
+	draw_triangle(length)
+	pen.rt(120)
+	pen.fd(length)
+	draw_triangle(length) 
+	pen.lt(120)
+	pen.fd(length)
+	draw_triangle(length)
+	pen.fd(length)
 
-sierpinski(5, 37.5)
+def sierpinski_order_two_recursive(n, length):
+	if n == 1:
+		draw_triangle(length)
+	else:
+		sierpinski_order_two_recursive(n - 1, length) # this is just like calling draw_triangle()
+		pen.rt(120)
+		pen.fd(length)
+		sierpinski_order_two_recursive(n -1, length)
+		pen.lt(120)
+		pen.fd(length)
+		sierpinski_order_two_recursive(n - 1, length)
+		pen.fd(length)
+
+def sierpinksi_order_three_recursive(n, length):
+	if n == 1:
+		draw_triangle(length)
+	else:
+		sierpinksi_order_three_recursive(n - 1, length) # this is just like calling draw_triangle()
+		pen.rt(120)
+		pen.fd(length * (n - 1))
+		sierpinksi_order_three_recursive(n -1, length)
+		pen.lt(120)
+		pen.fd(length * (n - 1))
+		sierpinksi_order_three_recursive(n - 1, length)
+		pen.fd(length * (n - 1))
+
+def sierpinski_order_n_recursive(n , length):
+	if n == 1:
+		draw_triangle(length)
+	else:
+		sierpinski_order_n_recursive(n - 1, length) # this is just like calling draw_triangle()
+		pen.rt(120)
+		pen.fd(length * 2 ** (n - 2))
+		sierpinski_order_n_recursive(n -1, length)
+		pen.lt(120)
+		pen.fd(length * 2 ** (n - 2))
+		sierpinski_order_n_recursive(n - 1, length)
+		pen.fd(length * 2 ** (n - 2))
+
+# sierpinski(5, 37.5)
 # eqilateral_triangle(100)
-pen.rt(100000)
+sierpinski_order_n_recursive(4,80)
+# pen.fd(1000)
+pen.rt(1000)
